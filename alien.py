9c2f833649edbeeaba2f715bf027c6025313f166
Dylan <dylan.cox25@students.bhasvic.ac.uk>
2026-08-17 16:20:20 +0100

    Added game container/class and aliens

---
+++ alien.py
@@ -0,0 +1,10 @@
+import pygame
+
+class Alien(pygame.sprite.Sprite):
+    def __init__(self, type, x, y):
+        super().__init__()
+
+        self.type = type
+        path = f"Images/Alien{type}.png"
+        self.image = pygame.image.load(path)
+        self.rect = self.image.get_rect(topleft = (x,y))
--- main.py
+++ main.py
@@ -1,45 +1,38 @@
 import pygame
-from spaceship import Spaceship
-from laser import Laser
-from obstacle import Obstacle
+from game import Game
+
 pygame.init()
 
 SCREEN_WIDTH = 700
 SCREEN_HEIGHT = 800
+BG_COLOR = (0,0,0)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Space Invaders")
 
-
-
-BG_COLOR = (0,0,0)
-
-
-
-spaceship = Spaceship(700, 800)
-spaceship_group = pygame.sprite.GroupSingle()
-spaceship_group.add(spaceship)
-
-obstacle = Obstacle()
-
-
 clock = pygame.time.Clock()
 run = True
+
+game = Game(SCREEN_WIDTH, SCREEN_HEIGHT)
 
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    # Update Spacship
-    spaceship_group.update()
+    # Updating
+    game.spaceship_group.update()
 
 
     # Drawing
     screen.fill(BG_COLOR)
-    spaceship_group.draw(screen)
-    spaceship_group.sprite.laser_group.draw(screen)
-    obstacle.block_group.draw(screen)
+    game.spaceship_group.draw(screen)
+    game.spaceship_group.sprite.laser_group.draw(screen)
+    
+    for obstacle in game.obstacles:
+        obstacle.block_group.draw(screen)
+
+    game.alien_group.draw(screen)
 
     pygame.display.update()
     clock.tick(60)
